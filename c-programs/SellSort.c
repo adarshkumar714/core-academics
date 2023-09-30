1bbbef0726f1a41f94acf2e6527efa686d1bbbc8
adarshkumar714 <adarshkumar20012704@gmail.com>
2023-09-30 21:32:16 +0530

    modified sellsort.c

--- c-programs/SellSort.c
+++ c-programs/SellSort.c
@@ -1,11 +1,14 @@
 // Time Complixity of Sell Sort
 // Best Case: nlogn
-// Average Case: nlog(n^2)
+// Average Case: n(log(n)^2)
 // Worst Case: n^2
 
 #include <stdio.h>
+#include <math.h>
 
 int main(){
+    // 33 31 40 8 12 17 25 42
+    // exception:  9 1 7 5 3 4
     int n;
     printf("Enter n: ");
     scanf("%d", &n);
@@ -20,9 +23,9 @@ int main(){
 
     int half = n/2;
     int j, temp, k;
-    for(j=0;j<n;j++){
+    for(j=0;j<(log(n)/log(2));j++){
         printf("\nhalf: %d\n", half);
-        for(i=0;i<n;i++){
+        for(i=0;i<(n-(half));i++){
             printf("\na[i]: %d, a[i+half]: %d\n", a[i], a[i+half]);
             if(a[i]>a[i+half]){
                 temp = a[i];
