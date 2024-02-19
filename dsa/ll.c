652d2afbe841674c01a655cc4916c9062ff1da28
adarshkumar714 <adarshkumar20012704@gmail.com>
2024-02-20 00:22:53 +0530

    made some functions again in ll.c, added graph.c and tree.c

--- dsa/ll.c
+++ dsa/ll.c
@@ -6,18 +6,28 @@ typedef struct node {
     struct node *next;
 } node;
 
+// return_type function_name(){
+//     ...
+// }
+
 node *createNode(int data){
+    printf("[+] createNode called\n");
     node *newNode = (node*) malloc(sizeof(struct node));
+    printf("[+] new node created\n");
     if(!newNode){
         printf("No memory available!\n");
         exit(1);
     }
+    // *newNode.data = data;
     newNode -> data = data;
+    printf("[+] newNode -> data = data\n");
     newNode -> next = NULL;
+    printf("[+] newNode -> next = NULL\n");
     return newNode;
 }
 
 void printLL(node *head){
+    printf("[+] linked list: ");
     node *current = head;
     while(current){
         printf("%d -> ", current->data);
@@ -50,6 +60,47 @@ void insertAfterNode(node *Node, int data){
     newNode->next = Node->next;
     Node->next = newNode;
 }
+
+// testing left
+node *deleteFirst(node *head){
+    node *temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
+
+// testing left
+void insertAtLast(node *head, int data){
+    node *newNode = createNode(data);
+
+    node *temp = head;
+    if(!temp) return;
+    else if(temp->next){
+        while(temp){
+            temp = temp->next;
+        }
+    }
+    temp->next = newNode;
+}
+
+// testing left
+void delete(node *Node){
+    node *temp = Node;
+    Node = Node->next;
+    free(temp);
+}
+
+// tested
+void printll2(node *head){
+    printf("[+] linked list: ");
+    node *temp = head;
+    while(temp!=NULL){
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+ 
 
 node *deleteFromStarting(node *head){
     if(head==NULL){
@@ -87,7 +138,7 @@ void deleteAfterNode(node *Node){
 //     return current;
 // }
 
-void reverseLL(node* *head){
+void reverseLL(node **head){
     node *prev = NULL;
     node *current = *head;
     node *next = NULL;
@@ -122,48 +173,58 @@ node *reverseLL_recursive(node *head){
     return rest;
 }
 
-
 int main(){
-    // creating a simaple linked list
-    node *head = createNode(1);
-    head -> next = createNode(2);
-    head -> next -> next = createNode(3);
-    printf("\n-- simple linked list --\n");
-    printLL(head);
+    printf("[+] started\n");
+    node *ll = createNode(1);
 
-    // inserting a node at beginning
-    head = insertAtStart(head, 0);
-    printf("\n-- insert 0 at starting --\n");
-    printLL(head);
+    insertAtLast(ll, 2);
 
-    // inserting a node after any node
-    insertAfterNode(head->next, 4);
-    printf("\n-- insert 4 after head->next --\n");
-    printLL(head);
-
-    // deleting a node from starting
-    head = deleteFromStarting(head);
-    printf("\n-- deleting head --\n");
-    printLL_recursive(head);
-
-    // deleting a node after head->next->next
-    deleteAfterNode(head->next->next);
-    printf("\n-- deleting a node after head->next->next --\n");
-    printLL_recursive(head);
-    
-    // reversing a linkedlist
-    reverseLL(&head);
-    printf("\n-- reversing a linkedlist --\n");
-    printLL_recursive(head);
-
-    // reversing a linkedlist (recursive)
-    head = reverseLL_recursive(head);
-    printf("\n-- reversing a linkedlist (recursive) --\n");
-    printLL_recursive(head);
-
-    printf("\n");
-
-    return 0;
+    printf("[+] created node\n");
+    printll2(ll);
+    printf("[+] program completed successfully\n");
 }
+
+// int main(){
+//     // creating a simaple linked list
+//     node *head = createNode(1);
+//     head -> next = createNode(2);
+//     head -> next -> next = createNode(3);
+//     printf("\n-- simple linked list --\n");
+//     printLL(head);
+
+//     // inserting a node at beginning
+//     head = insertAtStart(head, 0);
+//     printf("\n-- insert 0 at starting --\n");
+//     printLL(head);
+
+//     // inserting a node after any node
+//     insertAfterNode(head->next, 4);
+//     printf("\n-- insert 4 after head->next --\n");
+//     printLL(head);
+
+//     // deleting a node from starting
+//     head = deleteFromStarting(head);
+//     printf("\n-- deleting head --\n");
+//     printLL_recursive(head);
+
+//     // deleting a node after head->next->next
+//     deleteAfterNode(head->next->next);
+//     printf("\n-- deleting a node after head->next->next --\n");
+//     printLL_recursive(head);
+    
+//     // reversing a linkedlist
+//     reverseLL(&head);
+//     printf("\n-- reversing a linkedlist --\n");
+//     printLL_recursive(head);
+
+//     // reversing a linkedlist (recursive)
+//     head = reverseLL_recursive(head);
+//     printf("\n-- reversing a linkedlist (recursive) --\n");
+//     printLL_recursive(head);
+
+//     printf("\n");
+
+//     return 0;
+// }
 
 
