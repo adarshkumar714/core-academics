89b037398705126cfd5bbf4ab14d0725eb64ef6a
adarshkumar714 <adarshkumar20012704@gmail.com>
2024-05-14 23:21:27 +0530

    completed unit 1 python programs

---
+++ python/unit1_programs/data_types.py
@@ -0,0 +1,12 @@
+# Author: Adarsh Kumar
+
+'''
+# PYTHON DATA TYPES
+* Numeric -> int, float, complex numbers.
+* Dictionary
+* Boolean
+* Set
+* Sequence Type -> String, List, Tuple
+'''
+
+# ALL DATA TYPES ALREADY DISCUSED IN VARIABLES.PY
