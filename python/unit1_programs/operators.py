259d0d3d472d75e670751565736bb69d1744a3ee
adarshkumar714 <adarshkumar20012704@gmail.com>
2024-05-14 22:10:32 +0530

    started writing python programs of 2nd yr sem 4

---
+++ python/unit1_programs/operators.py
@@ -0,0 +1,45 @@
+# Author: Adarsh Kumar
+
+''' -------------------------------------------------- '''
+'''
+# BASIC PYTHON OPERATORS
+* Arithmetic operators
+* Assignment operators
+* Comparison operators
+* Logical operators
+'''
+
+# Arithmetic operators
+a = 3 + 3   # plus
+b = 6 - 2   # subtract
+c = 8 * 3   # multiplication
+d = 5 / 3   # division
+e = 9 % 7   # modulo
+f = 2 ** 3  # exponent -> 2 power of 3 -> 8
+g = 4 // 3  # floor division -> output : 1
+
+# Basic Assignment operators
+'''
+- = : equal
+- += : a+=1 -> a = a + 1
+- -= : a-=1 -> a = a - 1
+- *= : a*=2 -> a = a * 2
+- /= : a/=2 -> a = a / 2
+'''
+
+# Comparison operators
+'''
+- == : compair equality
+- != : not equal to
+- > : greater than
+- < : less than
+- >= : greater than equal to
+- <= : less than equal to
+'''
+
+# Logical operators
+'''
+* and : returns True if both statements are True
+* or : returns True if any one statements is True
+* not : reverse the statement
+'''
---
+++ python/unit1_programs/variables.py
@@ -0,0 +1,37 @@
+# Author : Adarsh Kumar
+
+''' -------------------------------------------------- '''
+''' ALL PYTHON VARIBALES '''
+a = 10 # int
+b = 12.4 # float
+c = "Adarsh Kumar"
+d = True # boolean
+e = ("Abhishek", "Akarsh", "Ishatva") # tuple
+f = [1,5,6] # list
+g = {
+    "name": "Nishu Maam",
+    "subject": "DSA",
+    "id": 218374913
+} # distionary
+h = {"a","b","c"} # set
+
+''' -------------------------------------------------- '''
+''' TYPE CASTING '''
+''' Implicit Type Casting '''
+A = a*b
+print(type(A)) # output: <class 'float'> | int converted to float automatically
+
+'''Explicit Type Casting'''
+print(int(b)) # output: 12 | float converted to int
+print(float(a)) # output: 10.0 | int converted to float
+
+''' -------------------------------------------------- '''
+''' FINDING TYPE OF A VARIABLE '''
+print(type(h)) # output: <class 'set'>
+
+
+
+
+
+
+
